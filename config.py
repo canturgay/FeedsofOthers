e18bc201c61f879e90119428e146f7cd7cc9bb9e
can.turgay <can.turgay@code.berlin>
2021-10-21 10:35:41 +0200

    def conf set /w config.py & instance folder

---
+++ config.py
@@ -0,0 +1,16 @@
+class Config(object):
+    TESTING = False
+    DEBUG = False
+
+class devConfig(Config):
+    FLASK_ENV = 'development'
+    TESTING = True
+    DB_SERVER = 'localserver'
+    DATABASE_URI =
+    
+
+class testConfig(Config):
+    DB_SERVER =
+    DATABASE_URI =
+    DEBUG = False
+    TESTING = True
--- feedsofothers/app.py
+++ feedsofothers/app.py
@@ -1,10 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from dotenv import load_dotenv, finddotenv
 
 # env var set
 load_dotenv(finddotenv())
-
 
 # initialize sql-alchemy
 db = SQLAlchemy()
@@ -12,7 +10,24 @@ db = SQLAlchemy()
 # create and initialize a new Flask app
 app = Flask(__name__)
 
+#configurations
+config = {
+    "development": "bookshelf.config.devConfig",
+    "testing": "bookshelf.config.testConfig",
+}
 
+def configure_app(app):
+    #Determine the configuration file to read using environment variables
+    config_name = os.getenv('FLASK_CONFIGURATION', 'development')
+    
+    #Read settings as objects
+    app.config.from_object(config[config_name])
+    
+    #Overwrite sensitive settings with the settings in the instance folder
+    app.config.from_pyfile('config.cfg', silent=True)
+
+
+#routing
 @app.route("/")
 def hello():
     return "Hello, World!"

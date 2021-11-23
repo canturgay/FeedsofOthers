31032e21abf3b60bd61809c808981033a61e6ead
can.turgay <can.turgay@code.berlin>
2021-11-23 17:40:13 +0100

    db helpers debug

--- backend/db_helpers.py
+++ backend/db_helpers.py
@@ -4,20 +4,6 @@ from flask_migrate import Migrate
 from sqlalchemy.engine import engine_from_config
 
 db = SQLAlchemy()
-
-class ENGINE_OPTIONS():
-    'pool_size'= 10,
-    'pool_recycle'= 120,
-    'pool_pre_ping'= True,
-    'echo'=True, 
-    'echo_pool'=False,
-    'pool_size'=10,
-    'pool_recycle'=3600,
-    'pool_timeout'=30,
-    'pool_pre_ping'=True
-
-
-
 base = db.make_declarative_base(db.Model)
 metadata_obj = db.metadata
 engine_config = { 'pool_size': 10, 'pool_recycle': 120, 'pool_pre_ping': True, 'echo': True, 'echo_pool': False, 'pool_size': 10, 'pool_recycle': 3600, 'pool_timeout': 30, 'pool_pre_ping': True}

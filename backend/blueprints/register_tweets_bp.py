fc02eb7cd9e276ac8227253ee1a97a2970d9aade
can.turgay <can.turgay@code.berlin>
2021-11-23 10:41:13 +0100

    started the function to register tweets to db

---
+++ backend/blueprints/register_tweets_bp.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, request, jsonify
+from backend.models import Tweet
+
+register_tweets = Blueprint('tweet_parse_bp', __name__)
+
+@register_tweets.route('/tweets', methods=['post'])
+def register_tweets():
+    data = request.get_json()
+    tweets = data['tweets']
+    tags = data['tags']
+
+    for tweet in tweets:
+

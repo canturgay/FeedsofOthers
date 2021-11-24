77e1d2ec25f7efd5a0892e3f6f57c9e8d80820ee
can.turgay <can.turgay@code.berlin>
2021-11-24 14:12:34 +0100

    db session var added

--- backend/db_helpers.py
+++ backend/db_helpers.py
@@ -1,3 +1,4 @@
+from re import S
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 from os import getenv
@@ -11,6 +12,6 @@ base = db.make_declarative_base(db.Model)
 engine = db.create_engine(getenv('DATABASE_URI'), {})    
 metadata = db.metadata
 migrate = Migrate(db=db, engine=engine)
-
+session = db.session
 
 

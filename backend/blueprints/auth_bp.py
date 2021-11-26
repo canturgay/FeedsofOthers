0ff7d1515cebb3ce5402964ce268c97d48a2ce3b
can.turgay <can.turgay@code.berlin>
2021-11-26 14:14:52 +0100

    unused line removed from register func

--- backend/blueprints/auth_bp.py
+++ backend/blueprints/auth_bp.py
@@ -15,25 +15,18 @@ auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
 @auth_bp.route('/register', methods=['POST'])
 def register():
     data = request.get_json()
+    print(data)
     user_id = data['user_id']
     oauth_access_token = data['oauth_access_token'] 
     oauth_access_token_secret = data['oauth_access_token_secret']
     oauth_callback_confirmed = data['oauth_callback_confirmed']
 
-    user = User.query.filter_by(id=id).first()
+    user = User.query.filter_by(id=user_id).first()
     try:
         if user is None:
-            user = User(id=id, oauth_token=oauth_token, oauth_token_secret=oauth_token_secret, oauth_callback_confirmed=oauth_callback_confirmed)
+            user = User(id=id, oauth_token=oauth_access_token, oauth_token_secret=oauth_access_token_secret, oauth_callback_confirmed=oauth_callback_confirmed)
             user.add()
-
-            access_token = create_access_token(identity=user.id)
-            refresh_token = create_refresh_token(identity=user.id)
-
-            response = jsonify()
-            set_access_cookies(response, access_token)
-            set_refresh_cookies(response, refresh_token)
-
-            return response, 201
+            return 201
         else:
             return redirect(url_for('auth_bp.login', data=request.get_json()))
     except:

edbb308a91692d69fd4f57f6c1d0f6be7acb3c37
can.turgay <can.turgay@code.berlin>
2021-11-29 13:43:45 +0100

    app context fixture implemented

--- backend/tests/app_test.py
+++ backend/tests/app_test.py
@@ -2,16 +2,11 @@ from os import getenv, environ
 
 environ['FLASK_CONFIGURATION'] = 'testing'
 
-from backend.app import app  
+from backend.app import create_app  
 import pytest
 from backend import models
-from backend.db_helpers import db, base
+from backend.db_helpers import base, db
 from datetime import datetime
-
-def test_secret():
-    assert app.config['SECRET_KEY'] == getenv('SECRET_KEY')
-
-
 
 @pytest.fixture(scope="session")
 def connection():
@@ -27,72 +22,78 @@ def setup_database(connection):
 
     base.metadata.drop_all()
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def db_session(connection):
     transaction = connection.begin()
     yield db.scoped_session(db.sessionmaker(autocommit=True, autoflush=True, bind=connection))
     transaction.rollback()
 
-@pytest.fixture
-def client(db_session):
-    client = app.test_client()
-    yield client
 
-with app.test_client() as tester:
-
-    def test_index():
-        with tester.get("/", content_type="html/text") as response:
-            assert response.status_code == 200, 304
-
-    def test_db_tables_exists():
-        assert db.inspect(models.User) is not None
-        assert db.inspect(models.Tag) is not None
-        assert db.inspect(models.Tweet) is not None
-        assert db.inspect(models.tags) is not None
-
-    def test_declarative_class_matches_db_table():
-        assert models.User.__table__ == db.inspect(models.User).local_table
-        assert models.Tag.__table__ == db.inspect(models.Tag).local_table
-        assert models.Tweet.__table__ == db.inspect(models.Tweet).local_table
-
-    def test_db_add_user(db_session):
-        session = db_session()
-        new_user = models.User(id=12300000, oauth_token='NPcudxy0yU5T3tBzho7iCotZ3cnetKwcTIRlX0iwRl0', oauth_token_secret='veNRnAWe6inFuo8o2u8SLLZLjolYDmDP7SzL0YfYI', authenticated=True)
-        session.add(new_user)
-        last = session.query(models.User).first()
-        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        last_created_at = last.created_at.strftime("%Y-%m-%d %H:%M:%S")
-        assert last == new_user
-        assert now == last_created_at
-
-    def test_register_user():
-        with tester.post("/auth/register", json={
-            "user_id": 12340000,
-            "oauth_access_token": "NPcudxy0yU5T3tBzho7iCotZ3cnetKwcTIRlX0iwRl0",
-            "oauth_access_token_secret" : "veNRnAWe6inFuo8o2u8SLLZLjolYDmDP7SzL0YfYI",
-            "oauth_callback_confirmed": True
-            }) as response:
-            assert response.status_code == 201
-            assert response.json == {'message': 'User registered'}
-
-    def test_register_user_with_correct_data(db_session):
-        session = db_session()
-        assert session.query(models.User).first().id == 12340000
-        assert session.query(models.User).first().oauth_token == 'NPcudxy0yU5T3tBzho7iCotZ3cnetKwcTIRlX0iwRl0'
-        assert session.query(models.User).first().oauth_token_secret == 'veNRnAWe6inFuo8o2u8SLLZLjolYDmDP7SzL0YfYI'
-        assert session.query(models.User).first().authenticated == True
-
-
-    def test_register_user_already_exists():
-        with tester.post("/auth/register", json={
-            "user_id": 12340000,
-            "oauth_access_token": "NPcudxy0yU5T3tBzho7iCotZ3cnetKwcTIRlX0iwRl0",
-            "oauth_access_token_secret" : "veNRnAWe6inFuo8o2u8SLLZLjolYDmDP7SzL0YfYI",
-            "oauth_callback_confirmed": True
-            }) as response:
-            assert response.status_code == 409
-            assert response.json == {'message': 'User already exists'}
-            
-
-
+@pytest.fixture(scope='session')
+def tester():
+    '''
+    Create a Flask app context for the tests.
+    '''
+    FoO_test = create_app()
+    tester = FoO_test.test_client()
+    ctx = FoO_test.app_context()
+    ctx.push()
     
+    yield tester
+
+    ctx.pop()
+
+
+def test_index(tester):
+    with tester.get("/", content_type="html/text") as response:
+        assert response.status_code == 200
+
+def test_db_tables_exists():
+    assert db.inspect(models.User) is not None
+    assert db.inspect(models.Tag) is not None
+    assert db.inspect(models.Tweet) is not None
+    assert db.inspect(models.tags) is not None
+
+def test_declarative_class_matches_db_table():
+    assert models.User.__table__ == db.inspect(models.User).local_table
+    assert models.Tag.__table__ == db.inspect(models.Tag).local_table
+    assert models.Tweet.__table__ == db.inspect(models.Tweet).local_table
+
+def test_db_add_user(db_session):
+    session = db_session()
+    new_user = models.User(id=12300000, oauth_token='NPcudxy0yU5T3tBzho7iCotZ3cnetKwcTIRlX0iwRl0', oauth_token_secret='veNRnAWe6inFuo8o2u8SLLZLjolYDmDP7SzL0YfYI', authenticated=True)
+    session.add(new_user)
+    last = session.query(models.User).first()
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    last_created_at = last.created_at.strftime("%Y-%m-%d %H:%M:%S")
+    assert last == new_user
+    assert now == last_created_at
+    session.delete(new_user)
+
+def test_register_user(tester):
+    with tester.post("/auth/register", json={
+        "user_id": 12340000,
+        "oauth_access_token": "NPcudxy0yU5T3tBzho7iCotZ3cnetKwcTIRlX0iwRl0",
+        "oauth_access_token_secret" : "veNRnAWe6inFuo8o2u8SLLZLjolYDmDP7SzL0YfYI",
+        "oauth_callback_confirmed": True
+        }) as response:
+        assert response.status_code == 201
+        assert response.json == {'message': 'User registered'}
+
+
+def test_register_user_already_exists(tester):
+    with tester.post("/auth/register", json={
+        "user_id": 12340000,
+        "oauth_access_token": "NPcudxy0yU5T3tBzho7iCotZ3cnetKwcTIRlX0iwRl0",
+        "oauth_access_token_secret" : "veNRnAWe6inFuo8o2u8SLLZLjolYDmDP7SzL0YfYI",
+        "oauth_callback_confirmed": True
+        }) as response:
+        assert response.status_code == 409
+        assert response.json == {'message': 'User already exists'}
+
+def test_registered_user_data(db_session):
+    assert db_session.query(models.User).first().id == 12340000
+
+        
+
+

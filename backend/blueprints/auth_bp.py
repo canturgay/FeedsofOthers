cfe5408b2627862ebbf2616cd58ef2ca25d18a94
can.turgay <can.turgay@code.berlin>
2021-11-29 10:03:07 +0100

    unused code in comments removed

--- backend/blueprints/auth_bp.py
+++ backend/blueprints/auth_bp.py
@@ -11,20 +11,13 @@ def register():
     oauth_access_token = data['oauth_access_token'] 
     oauth_access_token_secret = data['oauth_access_token_secret']
     oauth_callback_confirmed = data['oauth_callback_confirmed']
-
     
     try:
         user = User.query.filter_by(id=user_id).first()
         if user is None:
             user = User(id=user_id, oauth_token=oauth_access_token, oauth_token_secret=oauth_access_token_secret, authenticated=oauth_callback_confirmed)
-            db.session.add(user)
-            #access_token = create_access_token(identity=user.id)
-            #refresh_token = create_refresh_token(identity=user.id)
-            
+            db.session.add(user)         
             response = jsonify({'message': 'User registered'})
-            #set_access_cookies(response, access_token)
-            #set_refresh_cookies(response, refresh_token)
-
             return response, 201
         else:
             return redirect(url_for('auth_bp.login', data=request.get_json()))

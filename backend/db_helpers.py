2b7ac23bcc0327d5777c688b088002bf1c1b3d25
can.turgay <can.turgay@code.berlin>
2021-11-29 13:43:03 +0100

    database uri from config

--- backend/db_helpers.py
+++ backend/db_helpers.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 db = SQLAlchemy()
 base = db.make_declarative_base(db.Model)
-engine = db.create_engine(getenv('DATABASE_URI'), {})    
+engine = db.create_engine(getenv('SQLALCHEMY_DATABASE_URI'), {})    
 metadata = db.metadata
 session = db.session
 
